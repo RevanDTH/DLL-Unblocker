9afdf785608f518f66392289e27489ecfd9eb4ec
RevanDTH <72939884+RevanDTH@users.noreply.github.com>
2025-07-23 23:20:16 +0200

    Add interactive UI loop to unblock_dlls.ps1 script
    Introduces a simple menu-driven UI for unblocking DLL files, allowing users to repeatedly select actions or exit. The script now prompts for directory and recursion options within a loop, improving usability and error handling.

--- unblock_dlls.ps1
+++ unblock_dlls.ps1
@@ -15,11 +15,62 @@ function read_choice($question) {
     }
 }
 
+function UI() {
+    $userInput = $null
+
+    while ($userInput -ne 1 -and $userInput -ne 2) {
+        Clear-Host
+        Write-Host @'
+        (1) Unblock DLL files
+        (2) Exit
+'@
+        $userInput = Read-Host " "
+    }
+
+    if($userInput -eq 1){
+        return $true
+    }elseif ($userInput -eq 2) {
+        return $false
+    }
+}
+
 ## Main
 
-$directory_Path = Read-Host "Path of the Directory: "
-Clear-Host
-$recursive = Read-Host read_choice("Affect subfolders to?")
-Clear-Host
+$choice = UI
 
-Test-Path $directory_Path
+while($choice){
+
+    Clear-Host
+    $directory_Path = Read-Host "Path of the Directory "
+    Clear-Host
+    $recursive = read_choice("Affect subfolders to?")
+    Clear-Host
+    if($recursive){
+        $DLLs = Get-ChildItem $directory_Path -Recurse -Filter *.dll -File
+    }else{
+        $DLLs = Get-ChildItem $directory_Path -Filter *.dll -File   
+    }
+    if(Test-Path $directory_Path){
+        foreach($file in $DLLs){
+            try {
+                Write-Host "[INFO] Unblocking $file . . . `n"
+                $adsPath = $file.FullName + ":Zone.Identifier"
+                [System.IO.File]::Delete($adsPath)
+            }
+            catch {
+                Write-Error "[ERROR] Something went Wrong . . ."
+                Write-Warning "[INFO] Moving on to the next . . ."
+                Write-Error $_
+            }
+        }
+    }else{
+        Write-Error "[ERROR] The Path doesn't exist, please check if the Path is correct . . ."
+    }
+
+    Write-Host "[INFO] All DLLs unblocked"
+    Read-Host "Press ENTER to continue"
+
+    $choice = UI
+    if($choice -eq $false){exit}
+}
+
